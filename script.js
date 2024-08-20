4def3d51344e05c2d436406fdc07b71e0fdeb9e1
Phillip Martinez <phillip.martinez210@gmail.com>
2024-08-20 14:49:20 -0500

    Updated event listner to be async. Add showMealData() function to eventually output mealData in the browser for the user.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const randomMealURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+const newRecipeButton = document.getElementById("newRecipeButton");
 
 async function getMealData() {
     try {
@@ -10,10 +11,31 @@ async function getMealData() {
 
         const mealData = await response.json();
         console.log(mealData);
+        // console.log(mealData.meals[0].strMeal);
+        return mealData;
     }
     catch (error) {
         console.log(error);
+        return null;
     }
-}
+};
 
-getMealData();
+function showMealData(mealData) {
+    if (mealData) {
+        console.log(`Meal Name is: ${mealData.meals[0].strMeal}`);
+    } else {
+        console.log("No meal name available.");
+    }
+    
+};
+
+
+
+newRecipeButton.addEventListener("click", async () => {
+    const mealData = await getMealData();
+    showMealData(mealData);
+});
+
+
+
+
